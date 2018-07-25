f9f0e0be97f556302dad439e71a9f20bc73c191f
Rodney Worch <rodneyworch@Rodneys-MacBook-Pro.local>
2018-07-25 17:22:06 -0600

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'The Fun House';
+  title = 'The little house of horrors';
 }
